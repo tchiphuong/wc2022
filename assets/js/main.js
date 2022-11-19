c6a406b0fa89055b00f46f01625f659916bbefcf
Phuong Tran <chiphuong9299@hotmail.com>
2022-11-20 02:26:54 +0700

    commit code

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,7 +11,7 @@ $(function () {
         success: callAPI,
     });
 
-    new WorldCup(lstUrl);
+    var wc = new WorldCup(lstUrl);
     $("#list-group li").on("click", function () {
         $(this)
             .parents("ul")
@@ -29,6 +29,9 @@ $(function () {
         $("#list-stading li").hide();
         $(`${$(this).find("a").attr("target-link")}`).show();
     });
+    $(".team-item").on("click", function () {
+        wc.GetSquad(lstUrl.squad.url, lstUrl.squad.url1, $(this).attr("id"));
+    });
 });
 
 class WorldCup {
@@ -37,6 +40,7 @@ class WorldCup {
         this.GetGroup(this.lstUrl.group.url);
         this.GetStanding(this.lstUrl.standing.url);
         this.GetFixture(this.lstUrl.fixture.url);
+        this.GetTeam(this.lstUrl.team.url);
     }
     GetFixture(url) {
         $.ajax({
@@ -47,19 +51,24 @@ class WorldCup {
             success: function (response) {
                 $("#fixtures").empty();
                 response.Results.forEach((element) => {
+                    const isStarted =
+                        element.Home !== null &&
+                        element.Home.Score !== null &&
+                        element.Away !== null &&
+                        element.Home.Score !== null;
                     $("#fixtures").append(`
                         <li class="relative">
                             <button data-dropdown-toggle="${element.IdMatch}"
-                                class="text-white w-full font-medium rounded-lg text-sm text-center inline-flex items-center text-gray-800 border-gray-200 rounded-t-xl dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800"
+                                class="text-white w-full h-full font-medium rounded-lg text-sm text-center inline-flex items-center text-gray-800 border-gray-200 rounded-t-xl dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800"
                                 type="button">
                                 <div
-                                    class="block w-full p-6 pt-0 bg-white border border-gray-200 rounded-lg shadow-md relative hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-                                    <div class="text-lg my-4 pb-2 border-b-2">${
+                                    class="block w-full h-full p-6 pt-0 bg-white border border-gray-200 rounded-lg shadow-md relative hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                                    <div class="text-lg my-4 pb-2 border-b-2 dark:border-gray-700 dark:text-gray-100">${
                                         element.GroupName.length > 0
                                             ? element.GroupName[0].Description
                                             : element.StageName[0].Description
                                     }</div>
-                                    <div class="flex">
+                                    <div class="flex dark:text-gray-100">
                                         <div class="w-4/12 flex flex-col items-center">
                                             <img class="md:h-8 h-10 border border-yellow-500" src="${
                                                 element.Home !== null
@@ -76,19 +85,24 @@ class WorldCup {
                                                     : ""
                                             }</span>
                                         </div>
-                                        <div class="flex-grow flex items-center justify-center">
-                                            <span class="md:text-lg lg:text-2xl text-3xl p-1">0</span>
+                                        
+                                        ${
+                                            isStarted
+                                                ? `<div class="flex-grow flex items-center justify-center">
+                                            <span class="md:text-lg lg:text-2xl text-3xl p-1">${element.Home.Score}</span>
                                             <span class="md:text-lg lg:text-2xl text-3xl p-1">-</span>
-                                            <span class="md:text-lg lg:text-2xl text-3xl p-1">0</span>
-                                        </div>
-                                        <div class="flex-grow flex flex-col items-center justify-center hidden">
-                                            <span class="text-lg p-1 pb-0">${moment(
-                                                element.Date
-                                            ).format("L")}</span>
-                                            <span class="text-md p-1">${moment(element.Date).format(
-                                                "LT"
-                                            )}</span>
-                                        </div>
+                                            <span class="md:text-lg lg:text-2xl text-3xl p-1">${element.Away.Score}</span>
+                                        </div>`
+                                                : `<div class="flex-grow flex flex-col items-center justify-center">
+                                        <span class="text-lg p-1 pb-0">${moment(
+                                            element.Date
+                                        ).format("L")}</span>
+                                        <span class="text-md p-1">${moment(element.Date).format(
+                                            "LT"
+                                        )}</span>
+                                    </div>`
+                                        }
+                                        
                                         <div class="w-4/12 flex flex-col items-center">
                                             <img class="md:h-8 h-10 border border-yellow-500" src="${
                                                 element.Away !== null
@@ -106,7 +120,12 @@ class WorldCup {
                                             }</span>
                                         </div>
                                     </div>
-                                    <a href="../live" class="bg-red-100 text-red-800 text-xs font-semibold mr-2 px-2.5 py-0.5 top-4 left-3 rounded dark:bg-red-200 dark:text-red-900 absolute">LIVE</a>
+                                    ${
+                                        isStarted
+                                            ? '<a href="./live" class="bg-red-100 text-red-800 text-xs font-semibold mr-2 px-2.5 py-0.5 top-4 left-3 rounded dark:bg-red-200 dark:text-red-900 absolute">LIVE</a>'
+                                            : ""
+                                    }
+                                    
                                 </div>
                             </button>
                             <div id="${element.IdMatch}"
@@ -172,7 +191,7 @@ class WorldCup {
                         index > 0 ? "class='hidden'" : ""
                     }>
                             <div class="overflow-x-auto relative">
-                                <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400 border">
+                                <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400 border dark:border-gray-700">
                                     <thead
                                         class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                                         <tr>
@@ -257,6 +276,74 @@ class WorldCup {
                                 ${element.Points}
                             </td>
                         </tr>
+                    `);
+                });
+            },
+        });
+    }
+    GetTeam(url) {
+        $.ajax({
+            type: "get",
+            url: url,
+            async: false,
+            dataType: "json",
+            success: function (response) {
+                response.Results.forEach((element) => {
+                    $("#list-team").append(`
+                        <li id="${element.IdTeam}"
+                            class="flex flex-col items-center block px-5 py-4 text-sm font-medium text-center rounded-lg shadow cursor-pointer team-item md:py-7 hover:bg-gray-100 dark:text-gray-100 dark:bg-gray-800 dark:hover:bg-gray-700"
+                            type=" button" data-modal-toggle="model-squad">
+                            <img class="h-16 border border-yellow-500 md:h-20"
+                                src="${element.PictureUrl.replace("{size}", 4).replace(
+                                    "{format}",
+                                    "sq"
+                                )}" alt="${element.ShortClubName}">
+                            <span class="pt-2.5">${element.ShortClubName}</span>
+                        </li>
+                    `);
+                });
+            },
+        });
+    }
+    GetSquad(url, url1, test) {
+        const url2 = `${url}${test}${url1}`;
+        const listColor = {
+            Goalkeeper: "yellow",
+            Defender: "blue",
+            Midfielder: "green",
+            Forward: "red",
+        };
+        $.ajax({
+            type: "get",
+            url: url2,
+            async: false,
+            dataType: "json",
+            success: function (response) {
+                $("#team-info").html(`
+                    <img class="h-10 border border-yellow-500"
+                    src="${response.PictureUrl.replace("{size}", 4).replace("{format}", "sq")}"
+                    alt="">
+                    <span class="px-4 py-2">${response.TeamName[0].Description}</span>
+                `);
+                $("#list-squad").empty();
+                response.Players.forEach((element) => {
+                    const color = listColor[element.PositionLocalized[0].Description];
+                    const imgPlayer =
+                        element.PlayerPicture.PictureUrl != null
+                            ? `${element.PlayerPicture.PictureUrl}?io=transform:fill,width:256,height:128`
+                            : "./assets/img/player.png";
+                    $("#list-squad").append(`
+                        <li class="flex flex-col border-2 border-${color}-500 rounded-lg shadow hover:bg-gray-100 dark:text-gray-100 dark:bg-gray-800 dark:hover:bg-gray-700">
+                            <div class="relative flex justify-center">
+                                <span class="absolute md:text-2xl top-1 left-1 md:top-2 md:left-2">${
+                                    element.JerseyNum
+                                }</span>
+                                <img class="right-0 object-cover h-20"
+                                    src="${imgPlayer}"
+                                    alt="${element.ShortName[0].Description.toLowerCase()}" style="aspect-ratio: 1/1;">
+                            </div>
+                            <div class="flex items-center justify-center h-full text-xs text-center capitalize">${element.ShortName[0].Description.toLowerCase()}</div>
+                        </li>
                     `);
                 });
             },
