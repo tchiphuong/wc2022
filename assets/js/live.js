cd2d6ff95a56b082b7a279b028a427086651ee52
Phuong Tran <chiphuong9299@hotmail.com>
2022-11-22 23:05:53 +0700

    update channel

--- assets/js/live.js
+++ assets/js/live.js
@@ -12,8 +12,9 @@ $(function () {
 
     $("#list-channels").empty();
     lstChannel.forEach((element, index) => {
-        if (index === 0) {
-            $("#list-channels").append(`
+        if (element.isShow) {
+            if (index === 0) {
+                $("#list-channels").append(`
                 <li class="flex flex-col">
                     <button id="${element.name}" type="button"
                         class="channel-item text-white bg-blue-600 h-16 flex flex-col items-center justify-center hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-700">
@@ -22,8 +23,8 @@ $(function () {
                     <div class="text-center">${element.title}</div>
                 </li>
             `);
-        } else {
-            $("#list-channels").append(`
+            } else {
+                $("#list-channels").append(`
                 <li>
                     <button id="${element.name}" type="button"
                         class="channel-item text-gray-900 w-full bg-white h-16 flex items-center justify-center border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
@@ -32,6 +33,7 @@ $(function () {
                     <div class="text-center">${element.title}</div>
                 </li>
             `);
+            }
         }
     });
     $(".channel-item").on("click", function () {
